b085e8d167de3b9a5ec18c78dcb70e48ed48122a
Gennaro Tedesco <gennarotedesco@gmail.com>
2021-05-31 00:10:25 +0200

    adapted display table options

--- cmd/display.go
+++ cmd/display.go
@@ -10,15 +10,26 @@ import (
 
 func displayInfo() {
 	filesList := getFiles()
-	totalFiles, _ := countFiles(filesList)
+	totalFiles, extCount := countFiles(filesList)
+	total := totalFiles["normal"] + totalFiles["hidden"]
+
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendRows([]table.Row{
-		{"Files:", fmt.Sprintf("%v regular + %v hidden", totalFiles["normal"], totalFiles["hidden"])},
+		{"Files:", fmt.Sprintf("%v regular + %v hidden (%v%% %v, %v%% %v, %v%% %v)",
+			totalFiles["normal"], totalFiles["hidden"],
+			100*extCount[0].Value/total, extCount[0].Key,
+			100*extCount[1].Value/total, extCount[1].Key,
+			100*extCount[2].Value/total, extCount[2].Key),
+		},
 	})
 	t.AppendSeparator()
 	t.SetStyle(table.StyleLight)
 	t.Style().Color.Row = text.Colors{text.FgCyan}
 	t.Style().Options.SeparateColumns = false
+	t.Style().Box.BottomLeft = "╰"
+	t.Style().Box.TopLeft = "╭"
+	t.Style().Box.TopRight = "╮"
+	t.Style().Box.BottomRight = "╯"
 	t.Render()
 }
